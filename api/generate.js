bafa97f3f8ebcbbcd86b7e64d997ced0d55e037d
galeria360 <galeria360.pl@gmail.com>
2026-02-12 23:13:18 +0100

    Update generate.js

--- api/generate.js
+++ api/generate.js
@@ -1,19 +1,5 @@
 import { ImageResponse } from '@vercel/og';
 
-export const config = {
-runtime: 'nodejs'
-};
+export const config = { runtime: 'nodejs' };
 
-export default function handler() {
-return new ImageResponse(
-(
-<div style={{ fontSize: 60, color: 'white', background: 'orange', width: '100%', height: '100%', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-DZIALA! ⭐⭐⭐⭐⭐
-</div>
-),
-{
-width: 600,
-height: 400
-}
-);
-}
+export default function () { return new ImageResponse( ( <div style={{ fontSize: 60, color: 'white', background: 'orange', width: '100%', height: '100%', display: 'flex', alignItems: 'center', justifyContent: 'center' }}> DZIALA! ⭐⭐⭐⭐⭐ </div> ), { width: 600, height: 400 } ); }
