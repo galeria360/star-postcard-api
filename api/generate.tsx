709151804de3445d452743971ddbcaffd6f70f71
galeria360 <galeria360.pl@gmail.com>
2026-02-12 22:58:30 +0100

    Update generate.tsx

--- api/generate.tsx
+++ api/generate.tsx
@@ -3,4 +3,4 @@ import { NextRequest } from 'next/server';
 
 export const config = { runtime: 'edge' };
 
-export default function handler(req: NextRequest) { return new ImageResponse( ( <div style={{ height: '100%', width: '100%', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', backgroundColor: 'white', fontSize: 60 }}> <div style={{ color: '#f97316' }}>⭐⭐⭐⭐⭐</div> <div style={{ marginTop: 20 }}>Projekt Działa!</div> </div> ), { width: 600, height: 400 } ); }
+export default function handler(req: NextRequest) { return new ImageResponse( ( <div style={{ height: '100%', width: '100%', display: 'flex', alignItems: 'center', justifyContent: 'center', backgroundColor: 'black', color: 'white', fontSize: 50 }}> TEST DZIALA ⭐⭐⭐⭐⭐ </div> ), { width: 600, height: 400 } ); }
