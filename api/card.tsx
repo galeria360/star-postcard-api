f55d54628df4d84832c3beab113d3dfde4e7653d
galeria360 <galeria360.pl@gmail.com>
2026-02-12 23:27:55 +0100

    Update card.tsx

--- api/card.tsx
+++ api/card.tsx
@@ -1,5 +1,6 @@
 export const config = { runtime: 'nodejs' };
 
 export default function handler(req, res) {
-res.status(200).send('<h1>Serwer działa!</h1><p>Jeśli to widzisz, mozemy wgrywac grafikę.</p>');
+res.setHeader('Content-Type', 'text/html');
+res.status(200).send('<h1>Status: Serwer Zyje!</h1><p>Skonfigurowalismy modul ES. Teraz mozemy wracac do grafiki.</p>');
 }
