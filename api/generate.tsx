38290324650e44849a8039b3c36d7e9236fbacb4
galeria360 <galeria360.pl@gmail.com>
2026-02-12 23:17:30 +0100

    Update generate.tsx

--- api/generate.tsx
+++ api/generate.tsx
@@ -1,14 +1,7 @@
 import { ImageResponse } from '@vercel/og';
 
-export const config = { runtime: 'edge' };
+export const config = {
+runtime: 'edge',
+};
 
-export default function () {
-return new ImageResponse(
-(
-<div style={{ fontSize: 60, color: 'white', background: 'orange', width: '100%', height: '100%', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-DZIALA! ⭐⭐⭐⭐⭐
-</div>
-),
-{ width: 600, height: 400 }
-);
-}
+export default function () { return new ImageResponse( ( <div style={{ display: 'flex', fontSize: 60, color: 'white', background: 'orange', width: '100%', height: '100%', alignItems: 'center', justifyContent: 'center', }} > DZIAŁA! ⭐⭐⭐⭐⭐ </div> ), { width: 600, height: 400, } ); }
